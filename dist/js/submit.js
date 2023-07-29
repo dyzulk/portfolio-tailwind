fd0c448f6b1d038de5e55f234ac0dc0400d62ac8
Muhammad Herdy Iskandar <66510723+dyzulk@users.noreply.github.com>
2023-07-29 21:02:09 +0700

    Update submit.js

--- dist/js/submit.js
+++ dist/js/submit.js
@@ -5,7 +5,9 @@ function kirimPesan() {
     var pesan = document.getElementById('message');
 
     var gabungan = '%F0%9D%90%8D%F0%9D%90%9A%F0%9D%90%A6%F0%9D%90%9A%20%3A%0A' + nama.value + '%0A%0A%F0%9D%90%84%F0%9D%90%A6%F0%9D%90%9A%F0%9D%90%A2%F0%9D%90%A5%20%3A%0A' + email.value + '%0A%0A%F0%9D%90%92%F0%9D%90%AE%F0%9D%90%9B%F0%9D%90%A3%F0%9D%90%9E%F0%9D%90%A4%20%3A%0A' + subject.value + '%0A%0A%F0%9D%90%8F%F0%9D%90%9E%F0%9D%90%AC%F0%9D%90%9A%F0%9D%90%A7%20%3A%0A' + pesan.value;
-
+    var token = "5853349592:AAHX9c-wlohgY4wxVB6qM1qfw-gcBC_-zu8"; 
+    var grup = "-891230687"; 
+    
     // Menggunakan variabel token dan grup dari PHP
     $.ajax({
         url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
