e32f9f4ce20bd85517b1791f36c81079141a24f9
Muhammad Herdy Iskandar <66510723+dyzulk@users.noreply.github.com>
2023-09-24 07:21:42 +0700

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,16 @@
-// Navbar Fixed
+// Fungsi untuk mengganti kunci situs dengan kunci yang telah Anda atur di sisi server
+function setHcaptchaSiteKey() {
+    var siteKey = "YOUR_SITE_KEY_HERE"; // Ganti dengan kunci situs hCaptcha Anda
+    var hcaptchaContainer = document.getElementById("hcaptcha-container");
+    hcaptchaContainer.innerHTML = ''; // Hapus elemen yang mungkin sudah ada
+    var hcaptchaDiv = document.createElement("div");
+    hcaptchaDiv.className = "h-captcha";
+    hcaptchaDiv.dataset.sitekey = siteKey;
+    hcaptchaContainer.appendChild(hcaptchaDiv);
+}
+
+// Panggil fungsi untuk mengatur kunci situs saat halaman dimuat
+window.addEventListener("load", setHcaptchaSiteKey);// Navbar Fixed
 window.onscroll = function () {
   const header = document.querySelector('header');
   const fixedNav = header.offsetTop;
